c3b8106abe126f785516eaa0971d01aa3bc549b6
karmdesai <krd37@njit.edu>
2021-05-10 01:47:03 -0400

    Initial commit

---
+++ docs/js/all.js
@@ -0,0 +1,17 @@
+(function ($) {
+    $('#mlbTable').DataTable( {
+        "ajax": "http://localhost:9080/api/v1/mlb_players",
+        "columns": [
+            { "data": "id" },
+            { "data": "Name" },
+            { "data": "Team" },
+            { "data": "Position" },
+            { "data": "Height_inches" },
+            { "data": "Weight_lbs" },
+            { "data": "Age" }
+
+
+        ]
+    } );
+
+})(jQuery);
